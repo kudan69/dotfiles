1e2606dffb34dc1abe88332886566bd656691517
Jeremy Kahne <jkahne@gmail.com>
2012-08-19 21:02:31 -0400

    customizations wip

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,8 @@
 alias reload!='. ~/.zshrc'
+alias ll='ls -lF'
+alias la='ls -alF'
+alias mv='mv -i'
+alias vimm='mvim -v'
+alias gvim='mvim -p'
+alias be="bundle exec"
+alias cpwd="pwd | ruby -pe'\$_.chomp!' | pbcopy"
