61dfe0226769136ad941d035da582a48c01d383e
jkahne <jkahne@gmail.com>
2013-01-16 18:47:15 -0500

    getting mvim to work the way I want.

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -4,8 +4,8 @@ else
   export PS1='%3~$(git_info_for_prompt)%# '
 fi
 
-export LSCOLORS="exfxcxdxbxegedabagacad"
-export CLICOLOR=true
+export CLICOLOR=1
+export LSCOLORS=gxfxcxdxbxegedabagacad
 
 fpath=($ZSH/zsh/functions $fpath)
 
@@ -15,29 +15,17 @@ HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 
-setopt NO_BG_NICE # don't nice background tasks
-setopt NO_HUP
-setopt NO_LIST_BEEP
-setopt LOCAL_OPTIONS # allow functions to have local options
-setopt LOCAL_TRAPS # allow functions to have local traps
-setopt HIST_VERIFY
-setopt SHARE_HISTORY # share history between sessions ???
-setopt EXTENDED_HISTORY # add timestamps to history
-setopt PROMPT_SUBST
-#setopt CORRECT
-setopt COMPLETE_IN_WORD
-setopt IGNORE_EOF
-
-setopt APPEND_HISTORY # adds history
-setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
-setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
-setopt HIST_REDUCE_BLANKS
-
-# don't expand aliases _before_ completion has finished
-#   like: git comm-[tab]
-setopt complete_aliases
-
-zle -N newtab
+setopt always_to_end            # When complete from middle, move cursor
+setopt append_history           # Append to history file, not overwrite it
+setopt auto_cd                  # If a command is not in the hash table, and there exists an executable directory by that name, perform the cd command to that directory.
+setopt complete_in_word         # Always auto complete, not just at the end
+setopt extendedglob             # Extra filename generation features
+setopt hist_ignore_dups         # Don't enter command in history if it is duplicate of previous command
+setopt hist_find_no_dups        # Don't display duplicates when searching commands
+setopt inc_append_history       # Append history item as soon as command is executed, rather than waiting until the shell is killed
+setopt nobeep                   # Kill the fscking beep!
+setopt nohup										# In general, we don't kill background jobs upon logging out
+setopt prompt_subst             # parameter expansion, command substitution and arithmetic expansion is performed in prompts
 
 bindkey '^K' kill-whole-line
 bindkey "^R" history-incremental-search-backward
