f2202d4a16eee3a1a84b3d9ccd9b6ffabdecf9a1
jkahne <jkahne@gmail.com>
2014-02-19 15:26:00 -0500

    add silver searcher, update many things

--- zsh/prompt2.zsh
+++ zsh/prompt2.zsh
@@ -1,65 +1,37 @@
-ruby_version() {
-  v=$(ruby -v | awk '{ printf("%.5s", $2) }')
-  echo -ne "$v"
+git_prompt_info () {
+  ref=$(git symbolic-ref HEAD 2>/dev/null) || return
+  echo " (%{\e[0;35m%}${ref#refs/heads/}%{\e[0m%})"
 }
 
-git_branch() {
-  echo -ne $(git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
-}
 
-git_dirty() {
-  st=$(git st 2>/dev/null | tail -n 1)
-  if [[ $st == "" ]]
-  then
-    echo " "
-  else
-    if [[ $st == "nothing to commit (working directory clean)" ]]
-    then
-      echo " "
+zelda () {
+  if (( $+commands[pmset] )) ; then
+    battery="$(pmset -g ps | awk 'NR==2' | perl -pe 's/.*?(\d+)%.*/\1/')"
+
+    if [[ $battery -lt 16 ]]; then
+      echo "#[blink]♥#[noblink]♡♡♡♡♡"
+    elif [[ $battery -lt 32 ]]; then
+      echo "♥♡♡♡♡♡"
+    elif [[ $battery -lt 48 ]]; then
+      echo "♥♥♡♡♡♡"
+    elif [[ $battery -lt 64 ]]; then
+      echo "♥♥♥♡♡♡"
+    elif [[ $battery -lt 80 ]]; then
+      echo "♥♥♥♥♡♡"
+    elif [[ $battery -lt 96 ]]; then
+      echo "♥♥♥♥♥♡"
     else
-      echo "*"
+      echo "♥♥♥♥♥♥"
     fi
   fi
 }
 
-git_pair() {
-  git pair -si
-}
-
-git_prompt_info () {
- ref=$(git symbolic-ref HEAD 2>/dev/null) || return
- echo "(%{\e[0;35m%}${ref#refs/heads/}%{\e[0m%})"
-}
-
-project_name () {
-  name=$(pwd | awk -F'projects/' '{print $2}' | awk -F/ '{print $1}')
-  echo $name
-}
-
-project_name_color () {
-  name=$(project_name)
-  echo "%{\e[0;35m%}${name}%{\e[0m%}"
-}
-
-unpushed () {
-  git cherry -v origin/$(git_branch) 2>/dev/null
-}
-
-need_push () {
-  if [[ $(unpushed) == "" ]]
-  then
-    echo " "
-  else
-    echo "%{\e[0;33m%}$%{\e[0m%}"
-  fi
-}
-
-export PROMPT=$'%{\e[0;36m%}%2/%{\e[0m%}/ ᗌ '
 set_prompt () {
-  export RPROMPT="$(git_prompt_info)$(git_dirty)$(need_push)"
+  export PROMPT="$(zelda)$(git_prompt_info)"$' %{\e[0;36m%}%2/%{\e[0m%}/ ᗌ '
 }
 
+# gets called every time to display
 precmd() {
- print -Pn "\e]0;%~\a"
- set_prompt
+  print -Pn "\e]0;%~\a"
+  set_prompt
 }
