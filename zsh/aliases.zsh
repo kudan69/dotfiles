17f1ae76e89f3b76e8e7763d8818d2753092382a
Dustin Kocher <dtkocher@gmail.com>
2014-09-29 00:22:23 -0400

    Fix up some things for personal use.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,14 +1,10 @@
-alias mv='mv -i'
-alias be="bundle exec"
-
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
 alias ......="cd ../../../../.."
 alias .......="cd ../../../../../.."
-#alias dash="cd /Users/jkahne/projects/ss;watch -n7 bundle exec bin/ss dashboard"
-alias dash="cd /Users/jkahne/projects/tcli;watch -n5 ./tcli.rb dash"
+
 alias rt="ctags -R *"
 
 # grc overides for ls
@@ -19,15 +15,6 @@ alias l="ls -AFG"
 # Recursively delete `.DS_Store` files
 alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
 
-# ROT13-encode text. Works for decoding, too! ;)
-alias rot13='tr a-zA-Z n-za-mN-ZA-M'
-
-# Ring the terminal bell, and put a badge on Terminal.app’s Dock icon
-# (useful when executing time-consuming commands)
-# usage:  > long_command & badge
-alias badge="tput bel"
-
-
 # ruby
 alias cpwd="pwd | ruby -pe'\$_.chomp!' | pbcopy"
 alias rshare="ruby -rubygems -e \"['thin', 'rack', 'socket'].each {|file| require file };"\
@@ -35,7 +22,9 @@ alias rshare="ruby -rubygems -e \"['thin', 'rack', 'socket'].each {|file| requir
 " use Rack::CommonLogger; run Rack::Directory.new(Dir.pwd) }\""
 alias duo='du -sh $(du -s * | sort -k1n | ruby -nae '\''puts '\$'F[1..-1].join("?")'\'')'
 
+# My personal ones
+alias zshrc="vi ~/.zshrc"
+alias la='ls -al'
+alias vimconfig='vi ~/.vim/common_config/key_mappings.vim'
+alias personal='cd ~/Dropbox/home_projects/'
 
-
-# Pipe my public key to my clipboard. Fuck you, pay me.
-alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
